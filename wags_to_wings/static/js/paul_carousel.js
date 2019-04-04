cabb4585d240628314e856ab218367017f0aa15d
Virginia Lash <virginialash1@gmail.com>
2019-04-04 16:42:12 -0400

    carousel implemented but not working

--- wags_to_wings/static/js/paul_carousel.js
+++ wags_to_wings/static/js/paul_carousel.js
@@ -1,4 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.carousel');
-  var instances = M.Carousel.init(elems, { fullWidth: true, indicators: true, numVisible: 1});
+console.log('Paul Carousel attached.');
+
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.carousel');
+	var instances = M.Carousel.init(elems, {
+		fullWidth: true,
+		indicators: true,
+		numVisible: 1
+	});
 });
+
+// document.addEventListener('DOMContentLoaded', function() {
+// 	var elems = document.querySelectorAll('.carousel');
+// 	var instances = M.Carousel.init(elems, {
+// 		fullWidth: true,
+// 		indicators: true,
+// 		numVisible: 1
+// 	});
+// });
