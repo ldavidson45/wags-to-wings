9e8408793754557b22ced586f84d0a957d2640f5
Liz Davidson <lizdavidson45@gmail.com>
2019-04-04 12:29:50 -0400

    Continue model work.

--- wags_to_wings/models.py
+++ wags_to_wings/models.py
@@ -5,4 +5,12 @@ class Product(models.Model):
   name = models.CharField(max_length=50)
   price = models.IntegerField()
   description = models.TextField()
+  image_url = models.CharField(max_length=300)
 
+class Cart_Item(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name=')
+  quantity = models.IntegerField()
+  cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
+
+class Cart(models.Model):
+  order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='cars')
