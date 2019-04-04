9a6e2397cc0e8bc111a8dbf6dedd7503bfc478cf
Paul Blake <pnblake@gmail.com>
2019-04-04 14:37:09 -0400

    import urls to django project from app

--- wags_to_wings/views.py
+++ wags_to_wings/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Order, Cart, Product, Cart_Item, Shipping_Detail
 
 # Create your views here.
+def product_list(request):
+  food_items = Product.objects.filter(category="food")
+  toy_items = Product.objects.filter(category="toy")
+  return render(request, 'wags_to_wings/product_list.html', {'food_items': food_items, 'toy_items': toy_items})
