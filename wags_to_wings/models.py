98cb35b0fceabedab0d4d422400dd0296207fb98
Liz Davidson <lizdavidson45@gmail.com>
2019-04-04 12:50:31 -0400

    Finish models

--- wags_to_wings/models.py
+++ wags_to_wings/models.py
@@ -8,9 +8,18 @@ class Product(models.Model):
   image_url = models.CharField(max_length=300)
 
 class Cart_Item(models.Model):
-  product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name=')
+  product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
   quantity = models.IntegerField()
   cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
 
 class Cart(models.Model):
-  order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='cars')
+  order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='cart')
+
+class Order(models.Model):
+  order_status: models.CharField(max_length=50)
+
+class Shipping_Detail(models.Model):
+  name = models.CharField(max_length=100)
+  address = models.TextField()
+  credit_card_number = models.CharField(max_length=20)
+  order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='shipping_detail')
