6273d0f6f9f9f6b77ae56568f3a8ee3325c59081
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-04-05 10:55:41 -0400

    chat added

--- wags_to_wings/static/js/alsia_fab.js
+++ wags_to_wings/static/js/alsia_fab.js
@@ -7,9 +7,27 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 console.log("fab_attached!");
 
+// document.addEventListener("DOMContentLoaded", function() {
+//   var elems = document.querySelectorAll(".fixed-action-btn");
+//   var instances = M.FloatingActionButton.init(elems, {
+//     direction: "top",
+//     hoverEnabled: false
+//   });
+// });
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   var elems = document.querySelectorAll(".chat");
+//   var instances = M.FloatingActionButton.init(elems, {
+//     direction: "left",
+//     hoverEnabled: true
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", function() {
-  var elems = document.querySelectorAll(".fixed-action-btn");
-  var instances = M.FloatingActionButton.init(elems, {
-    direction: "top"
+  var elems = document.querySelectorAll(".chat-trigger");
+  var instances = M.Dropdown.init(elems, {
+    alignment: "right",
+    coverTrigger: false,
+    hover: true
   });
 });
