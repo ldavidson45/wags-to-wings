cf522aa69bbd7e52c442e1aae31ef97dbc366247
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-04-05 09:08:13 -0400

    dropdown button works

--- wags_to_wings/static/js/alsia_fab.js
+++ wags_to_wings/static/js/alsia_fab.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
   var elems = document.querySelectorAll(".dropdown-trigger");
-  var instances = M.Dropdown.init(elems, options);
+  var instances = M.Dropdown.init(elems, {
+    closeOnClick: true
+  });
+  instances.open();
 });
-console.log("attached!");
+console.log("fab_attached!");
