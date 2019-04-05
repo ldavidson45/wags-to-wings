eb4f37e8a2dcae8f1d1a0a90ce4c138fb8f3aaf0
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-04-05 09:52:55 -0400

    float logic added

--- wags_to_wings/static/js/alsia_fab.js
+++ wags_to_wings/static/js/alsia_fab.js
@@ -6,3 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 console.log("fab_attached!");
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".fixed-action-btn");
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: "top"
+  });
+});
